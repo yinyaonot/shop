8e4c2212c0fd6bae17aef16eec56a136e664f38b
yinyao <yinyaowhy@gmail.com>
2018-11-26 13:43:03 +0800

    four

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# shop
+readme
--- shopping91/urls.py
+++ shopping91/urls.py
@@ -1,8 +1,6 @@
-
-from django.contrib import admin
 from django.conf.urls import url
+from django.contrib import admin
 
 urlpatterns = [
     url('admin/', admin.site.urls),
-    url('^$')
 ]
